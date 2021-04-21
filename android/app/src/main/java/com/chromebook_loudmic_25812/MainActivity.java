ef56e7f3af6a9c29bd367e0e5f341c178237b172
Crowdbotics <team@crowdbotics.com>
2021-04-21 02:24:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chromebook_loudmic_25812/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chromebook_loudmic_25812;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "chromebook_loudmic_25812";
+  }
+}
